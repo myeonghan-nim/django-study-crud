eac8de7812cd82164b0fa48155c6075bf6be3a9d
myeonghan-nim <ataraxia9408@gmail.com>
2021-08-04 11:03:04 +0900

    Change: response logic

--- todos/urls.py
+++ todos/urls.py
@@ -1,19 +1,16 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    # read
     path('', views.index),
     path('<int:id>/detail/', views.detail),
-    
-    # create
+
     path('new/', views.new),
     path('create/', views.create),
 
-    # delete
     path('<int:id>/delete/', views.delete),
 
-    # update
     path('<int:id>/edit/', views.edit),
     path('<int:id>/update/', views.update),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,28 +1,19 @@
 from django.shortcuts import render, redirect
-from .models import Todo
 
-# Create your views here.
+from .models import Todo
 
 
 def index(request):
-
-    todos = Todo.objects.all()
-
     context = {
-        'todos': todos,
+        'todos': Todo.objects.all(),
     }
-
     return render(request, 'index.html', context)
 
 
 def detail(request, id):
-
-    todo = Todo.objects.get(id=id)
-
     context = {
-        'todo': todo,
+        'todo': Todo.objects.get(id=id),
     }
-
     return render(request, 'detail.html', context)
 
 
@@ -31,42 +22,28 @@ def new(request):
 
 
 def create(request):
-
-    title = request.GET.get('title')
-    due_date = request.GET.get('duedate')
-
-    todo = Todo(title=title, due_date=due_date)
-    todo.save()
-
+    Todo(
+        title=request.GET.get('title'),
+        due_date=request.GET.get('duedate')
+    ).save()
     return redirect('/todos/')
 
 
 def delete(request, id):
-
-    todo = Todo.objects.get(id=id)
-    todo.delete()
-
+    Todo.objects.get(id=id).delete()
     return redirect('/todos/')
 
 
 def edit(request, id):
-
-    todo = Todo.objects.get(id=id)
-
     context = {
-        'todo': todo,
+        'todo': Todo.objects.get(id=id),
     }
-
     return render(request, 'edit.html', context)
 
 
 def update(request, id):
-
     todo = Todo.objects.get(id=id)
-
     todo.title = request.GET.get('title')
     todo.due_date = request.GET.get('duedate')
     todo.save()
-
     return redirect('/todos/')
-    
